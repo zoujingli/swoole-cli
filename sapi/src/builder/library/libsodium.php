17a3f420fa18054ec1636e88a58192710dead267
findnr <cym504875043@gmail.com>
2026-01-26 13:20:33 +0800

    Update libsodium.php
    ibsodium下载地址更新

--- sapi/src/builder/library/libsodium.php
+++ sapi/src/builder/library/libsodium.php
@@ -9,7 +9,7 @@ return function (Preprocessor $p) {
             // ISC License, like BSD
             ->withLicense('https://en.wikipedia.org/wiki/ISC_license', Library::LICENSE_SPEC)
             ->withHomePage('https://doc.libsodium.org/')
-            ->withUrl('https://download.libsodium.org/libsodium/releases/libsodium-1.0.18.tar.gz')
+            ->withUrl('https://github.com/jedisct1/libsodium/releases/download/1.0.18-RELEASE/libsodium-1.0.18.tar.gz')
             ->withFileHash('md5', '3ca9ebc13b6b4735acae0a6a4c4f9a95')
             ->withPrefix(LIBSODIUM_PREFIX)
             ->withConfigure('./configure --prefix=' . LIBSODIUM_PREFIX . ' --enable-static --disable-shared')
